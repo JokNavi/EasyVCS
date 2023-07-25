43758c8177761322cdd5db20250085a56a91537f
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-07-25 14:13:27 +0200

    Add file methods Patch
    - Rename Version class to Patch
    - Add from_file() constructor to Patch
    - Add to_file() method to Patch

--- src/patch.py
+++ src/patch.py
@@ -2,15 +2,22 @@ from pathlib import Path
 import difflib
 
 
-class Version:
+class Patch:
 
     def __init__(self, old_bytes: bytes, new_bytes: bytes, version: int) -> None:
         pass
 
+    @classmethod
+    def from_file(cls: object, path: Path):
+        pass
+    
     def apply_patch(self, old_bytes: bytes) -> bytes:
         pass
 
     def revert_patch(self, new_bytes: bytes) -> bytes:
+        pass
+
+    def to_file(self, dir: Path) -> None:
         pass
 
 if __name__ == "__main__":
