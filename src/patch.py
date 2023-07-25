46e13125fec600d2e2bc8bb2b2bc3c4b9d1f938f
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-07-25 13:57:35 +0200

    Clean patch.py and create empty methods

--- src/patch.py
+++ src/patch.py
@@ -1,40 +1,17 @@
 from pathlib import Path
+import difflib
 
 
 class Version:
 
-    def __init__(self, bound_file_path: Path, version: int) -> None:
-        with open(bound_file_path, "rb") as file:
-            content = file.read()
-        self._content = content
-        self._version = version
-    
-    def get_content(self) -> bytes:
-        return self._content
-    
-    def get_version(self) -> int:
-        return self._version
-    
-    def to_file(self, save_dir: Path) -> None:
-        with open(self._save_path(save_dir), "wb") as file:
-            file.write(self.get_content())
+    def __init__(self, old_bytes: bytes, new_bytes: bytes, version: int) -> None:
+        pass
 
-    def _save_path(self, save_dir: Path) -> Path:
-        return save_dir.joinpath(f"{self._version}.easy_version")
+    def apply_patch(self, old_bytes: bytes) -> bytes:
+        pass
 
-    def delete(self, save_dir: Path) -> None:
-        self._save_path(save_dir).unlink(True)
-   
+    def revert_patch(self, new_bytes: bytes) -> bytes:
+        pass
 
 if __name__ == "__main__":
-    BOUND_FILE_PATH = Path("Testing/test.txt")
-    SAVE_DIR = BOUND_FILE_PATH.parent.joinpath("easy_vcs")
-
-    VERSION_0 = Version(BOUND_FILE_PATH, 0)
-    assert VERSION_0.get_version() == 0
-    assert VERSION_0.get_content() == b"Version 1 content"
-
-    VERSION_0.to_file(SAVE_DIR)
-    assert Path("./Testing/easy_vcs/").exists()
-
-    VERSION_0.delete(SAVE_DIR)
+    pass
