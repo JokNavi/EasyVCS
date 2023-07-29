8b59df7a5d5da067c9cf92a1d85e2813036cb512
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-07-29 19:04:58 +0200

    Refactor PatchManager
    - Refactor PatchManager's methods.
    - Add new_version() method to PatchManager
    - Add tests

--- Testing/patch_manager_tests.py
+++ Testing/patch_manager_tests.py
@@ -32,12 +32,29 @@ class TestPatchManager(unittest.TestCase):
         PATCH_0.to_file(PATCH_DIR)
         PATCH_1.to_file(PATCH_DIR)
         MANAGER = PatchManager(PATCH_DIR, 0)
-        
-        self.assertEquals(MANAGER.get_version(0), b"AAA")
-        self.assertEquals(MANAGER.get_version(1), b"AAABBB")
+
+        self.assertEqual(MANAGER.get_version(0), b"AAA")
+        self.assertEqual(MANAGER.get_version(1), b"AAABBB")
 
         MANAGER.clear_patches()
         PATCH_DIR.rmdir()
+
+    def test_new_version(self):
+        PATCH_DIR = Path("Files/versions")
+        PATCH_DIR.mkdir(True)
+
+        PATCH_0 = Patch.new(b"", b"AAA", 0)
+        PATCH_0.to_file(PATCH_DIR)
+
+        MANAGER = PatchManager(PATCH_DIR, 0)
+        MANAGER.new_version(b"AAABBB")
+        
+        self.assertEqual(MANAGER.get_version(0), b"AAA")
+        self.assertEqual(MANAGER.get_version(1), b"AAABBB")
+
+        MANAGER.clear_patches()
+        PATCH_DIR.rmdir()
+
 
         
 if __name__ == "__main__":
--- src/patch_manager.py
+++ src/patch_manager.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from pathlib import Path
 import sys
 
@@ -9,32 +10,28 @@ from patch import Patch
 class PatchManager:
     def __init__(self, dir: Path, loaded_version=0) -> None:
         self.patches_dir: Path = dir
-        self.loaded_patch: int = loaded_version
         self.patches: list[Patch] = self._read_patches()
+        
 
-    def _read_patches(self):
-        return [
-            Patch.from_file(Path(os.path.join(self.patches_dir, patch_path)))
-            for patch_path in os.listdir(self.patches_dir)
-            if os.path.isfile(os.path.join(self.patches_dir, patch_path))
-        ]
+    def _read_patches(self) -> list[Patch]:
+        patches = []
+        for patch_path in glob.glob(os.path.join(self.patches_dir, "*")):
+            patch = Patch.from_file(Path(patch_path))
+            patches.append(patch)
+        return patches
 
-    def current_version_number(self) -> int:
-        return self.loaded_patch
-
-    def create_version(self, new_bytes: bytes):
-        pass
+    def new_version(self, new_bytes: bytes):
+        patch = Patch.new(self.get_version(len(self.patches) - 1), new_bytes, len(self.patches))
+        self.patches.append(patch)
+        patch.to_file(self.patches_dir)
 
     def get_version(self, version: int) -> bytes:
         file: bytes = b""
-        i = 0
-        while i <= version:
-            file = self.patches[i].apply_patch(file)
-            i += 1
+        for patch in self.patches[:version+1]:
+            file = patch.apply_patch(file)
         return file
-        
-
-
 
     def clear_patches(self):
-        return [Path(os.path.join(self.patches_dir, patch_path)).unlink() for patch_path in os.listdir(self.patches_dir)]
+        patch_files = glob.glob(os.path.join(self.patches_dir, "*"))
+        for patch_file in patch_files:
+            os.unlink(patch_file)
